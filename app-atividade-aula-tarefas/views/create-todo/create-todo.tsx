8382de296a3162e22aa02c3c35e58ec67461cd4b
Thiago Rodrigues <90068572+trsaints@users.noreply.github.com>
2026-03-11 23:31:12 +0000

    feat(view): create base entrypoint for todo registration

--- app-atividade-aula-tarefas/views/create-todo/create-todo.tsx
+++ app-atividade-aula-tarefas/views/create-todo/create-todo.tsx
@@ -1,18 +1,58 @@
-import { Button, StyleSheet, Text, TextInput, View } from "react-native";
-import { createTodoLabel, inputTodoLabel, todoListHeader } from "../../constants/messages";
+import { Pressable, StyleSheet, Text, TextInput, View } from "react-native";
+import { createTodoLabel, inputTodoLabel, todoListHeader } from "./labels";
 
 export function CreateTodo() {
-    return (
-        <View style={createTodo}>
-            <TextInput placeholder={inputTodoLabel} />
-            <Button title={createTodoLabel} />
-            <Text>{todoListHeader}</Text>
-        </View>
-    )
+  return (
+    <View style={createTodo}>
+      <View style={inputCreateForm}>
+        <TextInput style={inputCreateTodo} placeholder={inputTodoLabel} />
+
+        <Pressable style={buttonCreateTodo} onPress={() => {}}>
+          <Text style={buttonCreateTodoText}>{createTodoLabel}</Text>
+        </Pressable>
+      </View>
+    
+      <View>
+        <Text>{todoListHeader}</Text>
+      </View>
+    </View>
+  );
 }
 
-const { createTodo } = StyleSheet.create({
-    createTodo: {
-        padding: 30,
-    }
-})
+const {
+  createTodo,
+  inputCreateForm,
+  inputCreateTodo,
+  buttonCreateTodo,
+  buttonCreateTodoText,
+} = StyleSheet.create({
+  createTodo: {
+    padding: 30,
+    display: "flex",
+    flexDirection: "column",
+    gap: 8,
+  },
+  inputCreateForm: {
+    display: "flex",
+    flexDirection: "row",
+    gap: 8,
+  },
+  inputCreateTodo: {
+    borderColor: "#ccc",
+    borderWidth: 1,
+    borderRadius: 8,
+    paddingBlock: 4,
+    paddingInline: 8,
+  },
+  buttonCreateTodo: {
+    backgroundColor: "#007AFF",
+    borderRadius: 8,
+    paddingVertical: 10,
+    paddingHorizontal: 14,
+    alignItems: "center",
+  },
+  buttonCreateTodoText: {
+    color: "white",
+    fontWeight: "600",
+  },
+});
